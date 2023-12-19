7510c50711a53d19be63be85cd77023722b365ac
Mehran Mohammad Beiglou <mehranmb78@gmail.com>
2023-12-19 10:08:54 +0100

    config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import Products from './Products/Products'
-import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return <Products />
 }
 
--- src/Products/Products.tsx
+++ src/Products/Products.tsx
@@ -1,5 +1,9 @@
 function Products() {
-  return <div>hello</div>
+  return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+  )
 }
 
 export default Products
